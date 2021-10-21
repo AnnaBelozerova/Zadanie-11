e2db716ebae7a44bd54dfe6ce94e814ed6d62eb8
Анна Белозерова <bel.denisovna@gmail.com>
2021-10-21 10:57:04 +0300

    Добавьте файлы проекта.

---
+++ Задание 11/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation;
+            Console.WriteLine("Решим уравнение типа 0=kx+b:");
+            Console.WriteLine("Введите k");
+            equation.k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            equation.b = Convert.ToDouble(Console.ReadLine());
+
+            equation.Root();            
+            
+            Console.ReadKey();
+
+        }
+        struct Equation
+        {
+            public double b;
+            public double k;
+
+            public void Root()
+            {
+                if (b == 0 && k == 0)
+                {
+                    Console.WriteLine("Корень уравнения равен: 0");
+                }
+                else
+                {
+                    if (k == 0)
+                    {
+                        Console.WriteLine("Деление на 0!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Корень уравнения равен: {0}", -b / k);
+                    }
+                }
+                    
+            }
+        }
+    }
+}
